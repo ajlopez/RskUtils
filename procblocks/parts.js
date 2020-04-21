eefc2eadf93b4a6c32de48f133f0fe253f546cde
ajlopez <webmaster@ajlopez.com>
2020-04-21 08:40:40 -0300

    Analyzing block header parts

--- procblocks/parts.js
+++ procblocks/parts.js
@@ -8,13 +8,39 @@ const lines = text.split('\n');
 
 const nlines = lines.length;
 
+const counts = [];
+let nblocks = 0;
+
 for (let k = 0; k < nlines; k++) {
     const line = lines[k].trim();
+    
+    if (!line.length)
+        continue;
+    
+    nblocks++;
     
     const elements = line.split(',');
     
     const block = elements[elements.length - 1];
+
+    const parts = rlp.decode(Buffer.from(block, 'hex'));
     
-    console.log(rlp.decode(Buffer.from(block, 'hex')));
+    if (!parts.length)
+        continue;
+    
+    for (let j = 0; j < parts[0].length; j++)
+        if (parts[0][j] instanceof Buffer)
+            counts[j] = (counts[j] || 0) + parts[0][j].length;
 }
+
+console.log(nblocks);
+console.log(counts);
+
+let tsize = 0;
+
+for (let k = 0; k < counts.length; k++)
+    tsize += counts[k];
+
+console.log(tsize);
+
 
