1b27295b33f61d002f5d9b4188e4683cb0cc4bea
ajlopez <webmaster@ajlopez.com>
2021-02-17 15:10:00 -0300

    Updated messagelogs

--- proclogs/messagelogs.js
+++ proclogs/messagelogs.js
@@ -6,17 +6,41 @@ const filename = process.argv[2];
 const text = fs.readFileSync(filename).toString();
 const lines = text.split('\n');
 
+let lastQueueSize = null;
+
 for (let k = 0, l = lines.length; k < l; k++) {
     const line = lines[k].trim();
     
-    if (isBlockLine(line))
+    if (isMessageLineStart(line)) {
         console.log(line);
+        lastQueueSize = null;
+        
+        continue;
+    }
+    
+    if (isMessageLineEnd(line)) {
+        if (lastQueueSize)
+            console.log(lastQueueSize);
+        
+        console.log(line);
+        lastQueueSize = null;
+        
+        continue;
+    }
+    
+    if (line.indexOf('queue size') >= 0) {
+        lastQueueSize = line;
+        continue;
+    }
 }
 
-function isBlockLine(line) {
-    return line.indexOf('Get Task') >= 0 ||
-           line.indexOf('Start Task') >= 0 ||
-           line.indexOf('End Task') >= 0 ||
+function isMessageLineStart(line) {
+    return line.indexOf('Get task') >= 0 ||
+           line.indexOf('Start task') >= 0;
+}
+
+function isMessageLineEnd(line) {
+    return line.indexOf('End task') >= 0 ||
            line.indexOf('Process message') >= 0 ||
            line.indexOf('processed after') >= 0;
 }
